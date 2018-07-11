7bc4382b10086bd94667bf89eb380807984e7eb1
jeyakumarkalirajwipro <jeyakumar.kaliraj@wipro.com>
2018-07-11 11:27:18 +0800

    spring jdbc template changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -17,9 +19,12 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+//    TimeEntryRepository timeEntryRepository() {
+//        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
+
 
     @Bean
     public ObjectMapper jsonObjectMapper() {
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public interface TimeEntryRepository {
     TimeEntry create(TimeEntry timeEntry);
-    TimeEntry get(Long id);
     TimeEntry find(Long id);
     List<TimeEntry> list();
     TimeEntry update(Long id, TimeEntry timeEntry);
